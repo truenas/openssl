0ff38a5d8b4794a9f85458f8794cfeab2fc34bc5
Aiden <53726630+aiden3c@users.noreply.github.com>
2024-10-23 14:53:03 -0400

    NAS-131233 / 25.04 / Use FIPS 3.0.9 with OpenSSL 3.0.14 (#14)

--- pull.sh
+++ pull.sh
@@ -5,6 +5,8 @@ VERSION=3.0.14
 REVISION=1
 DEBIAN_SUFFIX='~deb12u2'
 
+#Most recent validated FIPS (https://openssl-library.org/source/)
+FIPS_VERSION=3.0.9
 
 wget http://deb.debian.org/debian/pool/main/$PACKAGE_FIRST_CHAR/$PACKAGE/${PACKAGE}_$VERSION-$REVISION$DEBIAN_SUFFIX.debian.tar.xz
 tar xf ${PACKAGE}_$VERSION-$REVISION$DEBIAN_SUFFIX.debian.tar.xz
@@ -13,6 +15,25 @@ rm ${PACKAGE}_$VERSION-$REVISION$DEBIAN_SUFFIX.debian.tar.xz
 wget http://deb.debian.org/debian/pool/main/$PACKAGE_FIRST_CHAR/$PACKAGE/${PACKAGE}_$VERSION.orig.tar.gz
 tar xf ${PACKAGE}_$VERSION.orig.tar.gz --strip 1
 rm ${PACKAGE}_$VERSION.orig.tar.gz
+
+mkdir CUSTOMFIPS
+cd CUSTOMFIPS
+wget https://www.openssl.org/source/openssl-${FIPS_VERSION}.tar.gz
+tar xf openssl-${FIPS_VERSION}.tar.gz
+rm openssl-${FIPS_VERSION}.tar.gz
+mv openssl-${FIPS_VERSION}/* .
+rm -rf openssl-${FIPS_VERSION}
+./Configure enable-fips
+make -j$(nproc)
+
+cd ..
+
+# inject our custom FIPS module/config. This places it above the override_dh_installchangelogs (the end of override_dh_auto_install-arch)
+sed -i '/^override_dh_installchangelogs/i \
+\t# install our custom FIPS provider\
+\tcp CUSTOMFIPS/providers/fips.so debian/tmp/usr/lib/$(DEB_HOST_MULTIARCH)/ossl-modules/fips.so\
+\tcp CUSTOMFIPS/providers/fipsmodule.cnf debian/tmp/usr/lib/ssl/fipsmodule.cnf\n' debian/rules
+
 
 sed -i '/CONFARGS *=/ s/$/ enable-fips/' debian/rules
 echo "usr/lib/ssl/fipsmodule.cnf" >> debian/openssl.install
